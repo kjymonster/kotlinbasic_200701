b2901724a6e89e9544c0b595e841e8c3525ad2d3
김재영 <kjymonster@naver.com>
2020-07-01 20:35:38 +0900

    Log 찍어보기

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_200701/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_200701/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.kotlinbasic_200701
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,6 +19,11 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "터치버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show()
 
             // (어디서 띄울지, 메시지, 길게 보여줄지/짧게 보여줄지) .show()->화면에 띄우기
+
+            Log.d("메인화면", "터치버튼 눌림")
+
+            Log.e("메인화면", "에러 로그 찍어보기")
+
 
         }
     }
