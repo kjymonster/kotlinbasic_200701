3a14b64eb52046fa864b253c01be5568e8cc9794
김재영 <kjymonster@naver.com>
2020-07-01 20:00:43 +0900

    터치 버튼 클릭 이벤트 부여 "터치 버튼을 눌렀습니다."

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_200701/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_200701/MainActivity.kt
@@ -2,10 +2,18 @@ package kr.co.tjoeun.kotlinbasic_200701
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {  //fun == method
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)   //R == res폴더
+
+        touchBtn.setOnClickListener {
+
+            //touchBtn이 눌리면 실행될 코드를 적는 { }
+            Toast.makeText(this, "터치버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
